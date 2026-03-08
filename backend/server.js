3f2ce3487e0fd72fc82ccc65703eaa20aac7cb92
Bhavesh Mishra <bhaveshmishra7666@gmail.com>
2026-03-08 11:42:30 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "library_db"
+});
+
+db.connect((err)=>{
+if(err){
+console.log(err);
+}else{
+console.log("Connected to MySQL");
+}
+});
+
+app.get("/",(req,res)=>{
+res.send("Server Running");
+});
+
+app.post("/addBook",(req,res)=>{
+
+const title = req.body.title;
+const author = req.body.author;
+
+const sql = "INSERT INTO books (title,author) VALUES (?,?)";
+
+db.query(sql,[title,author],(err,result)=>{
+if(err){
+console.log(err);
+}else{
+res.send("Book Added");
+}
+});
+
+});
+
+app.listen(5000,()=>{
+console.log("Server running on port 5000");
+});
+
+app.get("/books",(req,res)=>{
+
+db.query("SELECT * FROM books",(err,result)=>{
+if(err){
+console.log(err);
+}else{
+res.send(result);
+}
+});
+
+});
+
+app.delete("/deleteBook/:id",(req,res)=>{
+
+const id = req.params.id;
+
+db.query(
+"DELETE FROM books WHERE id = ?",
+[id],
+(err,result)=>{
+if(err){
+console.log(err);
+}else{
+res.send("Book Deleted");
+}
+}
+);
+
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,94 @@
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+
+  const [books,setBooks] = useState([]);
+
+  const [title,setTitle] = useState("");
+  const [author,setAuthor] = useState("");
+
+  const addBook = () => {
+    axios.post("http://localhost:5000/addBook",{
+      title:title,
+      author:author
+    })
+    .then(()=>{
+      alert("Book Added Successfully");
+    });
+  };
+
+  const deleteBook = (id) => {
+
+axios.delete("http://localhost:5000/deleteBook/"+id)
+.then(()=>{
+alert("Book Deleted");
+window.location.reload();
+});
+
+};
+
+  useEffect(()=>{
+axios.get("http://localhost:5000/books")
+.then((response)=>{
+setBooks(response.data);
+});
+},[]);
+
+  return (
+    <div className="container" style={{textAlign:"center",marginTop:"50px"}}>
+      <h1>Library Management System</h1>
+
+      <input
+        placeholder="Book Title"
+        onChange={(e)=>setTitle(e.target.value)}
+      />
+
+      <br/><br/>
+
+      <input
+        placeholder="Author"
+        onChange={(e)=>setAuthor(e.target.value)}
+      />
+
+      <br/><br/>
+
+      <button onClick={addBook}>Add Book</button>
+
+      <h2>Book List</h2>
+
+<table border="1" align="center">
+
+<tr>
+<th>ID</th>
+<th>Title</th>
+<th>Author</th>
+<th>Action</th>
+</tr>
+
+{books.map((book)=>{
+return(
+<tr key={book.id}>
+<td>{book.id}</td>
+<td>{book.title}</td>
+<td>{book.author}</td>
+
+<td>
+<button className="delete-btn" onClick={()=>deleteBook(book.id)}>
+Delete
+</button>
+</td>
+
+</tr>
+);
+})}
+
+</table>
+
+    </div>
+  );
+}
+
+export default App;
